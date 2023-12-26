44883e0a70428ad39774d36fe10590e66c93ef7c
srinivas gonepudi <srigonepudi@gmail.com>
2023-12-26 12:56:26 +0530

    added new instnace

--- instance.sh
+++ instance.sh
@@ -1,9 +1,12 @@
-AMI_iD=ami-03265a0778a880afb
+AMI_ID=ami-03265a0778a880afb
 SG_ID=sg-0eab7d3878626d44d
 INSTANCE_TYPE="t2.micro"
 SERVER_NAMES=("web" "catalogue" "cart" "user" "shipping" "payment" "mysql" "rabbitmq" "redis" "dispatch")
 
 for SERVER_NAME in "${SERVER_NAMES[@]}"; do
-    aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type t2.micro  --security-group-ids sg-0eab7d3878626d44d
-    echo "Instances have been created"
-done        
+    INSTANCE_ID=$(aws ec2 run-instances --image-id "$AMI_ID" --instance-type "$INSTANCE_TYPE" --security-group-ids "$SG_ID" --query 'Instances[0].InstanceId' --output text)
+
+    aws ec2 create-tags --resources "$INSTANCE_ID" --tags "Key=Name,Value=$SERVER_NAME"
+    
+    echo "Instance $SERVER_NAME has been created with ID: $INSTANCE_ID"
+done
